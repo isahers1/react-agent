b6254db7aec1f38db7594c4d5212ade5b96e96f6
Isaac Francisco <78627776+isahers1@users.noreply.github.com>
2025-11-07 14:34:37 -0800

    Update graph.py

--- src/react_agent/graph.py
+++ src/react_agent/graph.py
@@ -19,59 +19,59 @@ from langgraph.types import interrupt
 
 # Define the function that calls the model
 
-
-async def call_model(
-    state: State, runtime: Runtime[Context]
-) -> Dict[str, List[AIMessage]]:
-    """Call the LLM powering our "agent".
-
-    This function prepares the prompt, initializes the model, and processes the response.
-
-    Args:
-        state (State): The current state of the conversation.
-        config (RunnableConfig): Configuration for the model run.
-
-    Returns:
-        dict: A dictionary containing the model's response message.
-    """
-    foo = interrupt({"question":"Do you approve this action?"})
-    # Initialize the model with tool binding. Change the model or add more tools here.
-    model = load_chat_model(runtime.context.model).bind_tools(TOOLS)
-
-    # Format the system prompt. Customize this to change the agent's behavior.
-    system_message = runtime.context.system_prompt.format(
-        system_time=datetime.now(tz=UTC).isoformat()
-    )
-
-    # Get the model's response
-    response = cast( # type: ignore[redundant-cast]
-        AIMessage,
-        await model.ainvoke(
-            [{"role": "system", "content": system_message}, *state.messages]
-        ),
-    )
-
-    # Handle the case when it's the last step and the model still wants to use a tool
-    if state.is_last_step and response.tool_calls:
-        return {
-            "messages": [
-                AIMessage(
-                    id=response.id,
-                    content="Sorry, I could not find an answer to your question in the specified number of steps.",
-                )
-            ]
-        }
-
-    # Return the model's response as a list to be added to existing messages
-    return {"messages": [response]}
-
+def generate_model_node():
+    def call_model(
+        state: State, runtime: Runtime[Context]
+    ) -> Dict[str, List[AIMessage]]:
+        """Call the LLM powering our "agent".
+    
+        This function prepares the prompt, initializes the model, and processes the response.
+    
+        Args:
+            state (State): The current state of the conversation.
+            config (RunnableConfig): Configuration for the model run.
+    
+        Returns:
+            dict: A dictionary containing the model's response message.
+        """
+        foo = interrupt({"question":"Do you approve this action?"})
+        # Initialize the model with tool binding. Change the model or add more tools here.
+        model = load_chat_model(runtime.context.model).bind_tools(TOOLS)
+    
+        # Format the system prompt. Customize this to change the agent's behavior.
+        system_message = runtime.context.system_prompt.format(
+            system_time=datetime.now(tz=UTC).isoformat()
+        )
+    
+        # Get the model's response
+        response = cast( # type: ignore[redundant-cast]
+            AIMessage,
+            model.invoke(
+                [{"role": "system", "content": system_message}, *state.messages]
+            ),
+        )
+    
+        # Handle the case when it's the last step and the model still wants to use a tool
+        if state.is_last_step and response.tool_calls:
+            return {
+                "messages": [
+                    AIMessage(
+                        id=response.id,
+                        content="Sorry, I could not find an answer to your question in the specified number of steps.",
+                    )
+                ]
+            }
+    
+        # Return the model's response as a list to be added to existing messages
+        return {"messages": [response]}
+    return call_model
 
 # Define a new graph
 
 builder = StateGraph(State, input_schema=InputState, context_schema=Context)
 
 # Define the two nodes we will cycle between
-builder.add_node(call_model)
+builder.add_node(generate_model_node())
 builder.add_node("tools", ToolNode(TOOLS))
 
 # Set the entrypoint as `call_model`
